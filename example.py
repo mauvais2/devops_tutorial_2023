e0bae84883db5d6c9bb20e092dae411e48d2475d
Jessica Mauvais <mauvais2@llnl.gov>
2024-06-04 10:50:06 -0700

    corrected the comments

--- example.py
+++ example.py
@@ -13,27 +13,27 @@ class ArithmeticPair:
         self.x = x
         self.y = y
 
-#    def sum(self):
-#        """
-#        Compute :math:`x + y`.
-#        """
-#        #return self.x + self.y
+    def sum(self):
+        """
+        Compute :math:`x + y`.
+        """
+        return self.x + self.y
 
-#    def difference(self):
-#        """
-#        Compute :math:`x - y`.
-#        """
-#        #return self.y - self.x
+    def difference(self):
+        """
+        Compute :math:`x - y`.
+        """
+        return self.y - self.x
 
-#    def product(self):
-#        """
-#        Compute :math:`x * y`.
-#        """
-        #return self.x * self.y
+    def product(self):
+        """
+        Compute :math:`x * y`.
+        """
+        return self.x * self.y
 
-#    def quotient(self):
-#        """
-#        Compute :math:`x / y`.
-#        """
-#        #return self.x / self.y
+    def quotient(self):
+        """
+        Compute :math:`x / y`.
+        """
+        return self.x / self.y
     
